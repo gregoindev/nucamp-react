a41773d94fb86008bb45109f302ce9483375b4de
gregoindev <gregocodes23@gmail.com>
2022-05-07 17:07:36 -0700

    Week 1 Assignment

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from "react";
-// import logo from './logo.svg';
-// import { Counter } from './features/counter/Counter';
 import "./App.css";
+import CampsitesList from "./features/campsites/CampsitesList";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
-  return <div className="App">hello nucamp!</div>;
+  return (
+    <div className="App">
+      <Header />
+      <CampsitesList />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ import "typeface-open-sans";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
+// const root = React.DOM.createRoot(document.getElementById('root));
 
 root.render(
   <React.StrictMode>
