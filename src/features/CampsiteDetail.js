a59382b098647079577b3b9b0c271f2b7747d7e1
gregoindev <gregocodes23@gmail.com>
2022-05-11 19:07:52 -0700

    campsitesSlice

---
+++ src/features/CampsiteDetail.js
@@ -0,0 +1,7 @@
+import { Card, CArdImg, CardText, CardBody, Col } from "reactstrap";
+
+const CampsiteDetail = ({ campsite }) => {
+  const { image, name, description } = campsite;
+};
+
+export default CampsiteDetail;
