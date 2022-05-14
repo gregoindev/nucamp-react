f02f94d3d2583181e8d53a5932978d701675cc72
gregoindev <gregocodes23@gmail.com>
2022-05-14 15:12:35 -0700

    Week2 Assgnmnt

---
+++ src/features/partners/PartnerList.js
@@ -0,0 +1,24 @@
+import { Row, Col } from "reactstrap";
+import { PARTNERS } from "../../app/shared/PARTNERS";
+import Partner from "./Partner";
+import { selectAllPartners } from "./partnersSlice";
+
+const PartnersList = () => {
+  const partners = selectAllPartners();
+
+  return (
+    <Col className="mt-4">
+      <Row>
+        {PARTNERS.map((partner) => {
+          return (
+            <div className="d-flex mb-5" key={partner.id}>
+              <Partner partner={partner} />
+            </div>
+          );
+        })}
+      </Row>
+    </Col>
+  );
+};
+
+export default PartnersList;
